f750a239c6efe74eb6391db02e132b2907ce39e4
C29-bugologist <167443196+C29-bugologist@users.noreply.github.com>
2024-05-17 01:17:17 +0530

    Update GPA_Calculatour.py

--- GPA_Calculatour.py
+++ GPA_Calculatour.py
@@ -5,7 +5,7 @@ print("----------GPA Calculator----------\n\n")
 num_courses = int(input("Enter number of courses: "))
 
 for i in range(num_courses):
-  grade = input("Enter the letter grade for course " + str(i + 1) + ": ")
+  grade = input("Enter the letter grade for course(in UPPERCASE) " + str(i + 1) + ": ")
   credits = int(input("Enter the number of credits for course " + str(i + 1) + ": "))
   total_grade_points += grade_table[grade] * credits
   total_credits += credits
