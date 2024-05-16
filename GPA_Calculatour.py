6f058dfa6ff472f9bb4fd970429a11a7d639909f
C29-bugologist <167443196+C29-bugologist@users.noreply.github.com>
2024-05-16 20:12:36 +0530

    Create GPA_Calculatour.py

---
+++ GPA_Calculatour.py
@@ -0,0 +1,15 @@
+grade_table = {"A": 4, "B": 3, "C": 2, "D": 1}
+total_grade_points = 0
+total_credits = 0
+print("----------GPA Calculator----------\n\n")
+num_courses = int(input("Enter number of courses: "))
+
+for i in range(num_courses):
+  grade = input("Enter the letter grade for course " + str(i + 1) + ": ")
+  credits = int(input("Enter the number of credits for course " + str(i + 1) + ": "))
+  total_grade_points += grade_table[grade] * credits
+  total_credits += credits
+
+GPA = total_grade_points / total_credits
+
+print("GPA:", GPA)
